0c7643f60e1115340f34c4e3ef0cb7e90bd7f7e9
hj <mmjh9258@gmail.com>
2021-11-27 23:32:26 +0900

    front source add

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import RestAPI from "./RestAPI.js"
+import React from "react"
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+	  <RestAPI />
+      </header>	
+    </div> 
+  );
+}
+
+export default App;
---
+++ src/RestAPI.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import axios from "axios";
+//import "./RestAPI.css";
+
+function RestAPI() {
+  const [text, setText] = useState([]);
+
+  return (
+    <>
+      <h1>REST API 연습</h1>
+      <div className="btn-primary">
+        <button
+          onClick={() => {
+            axios
+              .post("http://work87.wiro.kr/api/board/post", {
+                title: "제목",
+                content: "내용",
+              })
+              .then(function (response) {
+                console.log(response);
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+          }}
+        >
+          POST
+        </button>
+        <button
+          onClick={() => {
+            axios
+              .get("http://work87.wiro.kr/api/quiz")
+              .then((response) => {
+                setText([...response.data]);
+                console.log(response.data);
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+          }}
+        >
+          GET
+        </button>
+      </div>
+      {text.map((e) => (
+        <div>
+          {" "}
+          <div className="list">
+            <span>
+              {e.id}번, {e.title}, {e.content}, {e.update_at}
+            </span>
+            <button
+              className="btn-delete"
+              onClick={() => {
+                axios.delete(`http://work87.wiro.kr/api/board/post/${e.id}`);
+                setText(text.filter((text) => text.id !== e.id));
+              }}
+            >
+              DELETE
+            </button>{" "}
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default RestAPI;
